4edfba1e7f70df9bcc344c6c7fd43d63075f6476
liyanfeng <liyanfeng@yingu.com>
2019-05-13 16:19:04 +0800

    first commit

---
+++ app/src/main/java/com/example/test1/TestBean.java
@@ -0,0 +1,34 @@
+package com.example.test1;
+
+public class TestBean {
+    private String name;
+    private int age;
+
+    public TestBean() {}
+
+    private TestBean(String name){
+        this.name = name;
+    }
+
+    public TestBean(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setMName(String name) {
+        this.name = name;
+    }
+
+    private void setNameAndAge(String name, int age) {
+        this.age = age;
+        this.name = name;
+    }
+}
